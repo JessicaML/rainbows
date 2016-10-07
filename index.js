de76cb5dc693983053b999a5a1216abcd16d9079
Jessica Leach <jessicamaryleach@gmail.com>
2016-10-07 14:40:58 +0200

    prints colors every second

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 colors = require('colors');
 
-console.log('hello'); // outputs text 
-console.log('hello'.green); // outputs green text 
-console.log('hello'.rainbow); // outputs rainbow text 
+var myString = "Hello world";
 
+function printColors() {
+
+
+    console.log(myString);
+    console.log(myString.yellow);
+    console.log(myString.green);
+    console.log(myString.cyan);
+    console.log(myString.blue);
+    console.log(myString.red);
+    console.log(myString.magenta);
+
+}
+setInterval(printColors, 1000);
